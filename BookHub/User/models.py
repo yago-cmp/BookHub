f90c342e43ffb4ea26fa318703395ea746910bcd
teutio <yagopcampello@gmail.com>
2026-03-11 23:13:17 -0300

    feat: app user e models user/profile

---
+++ BookHub/User/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.conf import settings
+import uuid
+# Create your models here.
+
+
+class User(models.Model):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(max_length=30)
+    password = models.CharField(max_length=16)
+
+    def __str__(self): 
+        return self.email
+    
+
+class Profile(models.Model):
+    #related name faz o acesso de profile atraves de user mais facil. por padrao ja e profile
+    user_id = models.OneToOneField('User', on_delete=models.CASCADE, related_name='profile')
+
+    profile_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=15, unique=True)
+    bio = models.TextField(blank=True, null=True)
+    pfp = models.ImageField(default='fallback.png', blank=True, null=True)
+    streak = models.IntegerField(default=0)
+    achievements = models.CharField(max_length=100, blank=True, null=True)
+    joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self): #diz pro painel de admin qual o nome do objeto
+        return self.username
