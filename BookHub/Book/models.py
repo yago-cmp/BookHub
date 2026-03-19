c0a7e0f1e42983304e02cc1b17999821d90131d9
teutio <yagopcampello@gmail.com>
2026-03-18 23:53:57 -0300

    feat: add book model

--- BookHub/Book/models.py
+++ BookHub/Book/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Book(models.Model):
+    author_id = models.OneToOneField('Author', related_name='book')
+
+    book_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=40)
+    pages = models.IntegerField(default=0)    
+    editor = models.CharField(max_length=30, blank = True, null = True)
+    genre = models.CharField(max_length=30, blank = True, null = True)
+    synopsys = models.TextField(blank=True, null=True)
+    edition = models.CharField(blank = True, null = True)
+    cover = models.ImageField(default='fallback.png', blank=True, null=True)
+    year = models.DateTimeField(blank = True, null = True)
+
+    def __str__(self): 
+        return self.title
