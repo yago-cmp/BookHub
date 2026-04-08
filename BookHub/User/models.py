e9e2de05147dd8b7af0c67eda332c73088353865
teutio <yagopcampello@gmail.com>
2026-04-07 23:47:54 -0300

    fix models

--- BookHub/User/models.py
+++ BookHub/User/models.py
@@ -6,7 +6,7 @@ import uuid
 
 
 class User(models.Model):
-    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(max_length=30)
     password = models.CharField(max_length=16)
 
@@ -15,9 +15,8 @@ class User(models.Model):
     
 class Profile(models.Model):
     #related name faz o acesso de profile atraves de user mais facil. por padrao ja e profile
-    user_id = models.OneToOneField('User', on_delete=models.CASCADE, related_name='profile')
-
-    profile_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField('User', on_delete=models.CASCADE, related_name='profile')
+    profile = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=15, unique=True)
     bio = models.TextField(blank=True, null=True)
     pfp = models.ImageField(default='fallback.png', blank=True, null=True)
@@ -29,7 +28,7 @@ class Profile(models.Model):
         return self.username
 
 class Follow(models.Model):
-    follow_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    follow = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     follower = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="following")
     following = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="followers")
 
@@ -40,33 +39,33 @@ class Follow(models.Model):
         return self.follower.username + ' follows ' + self.following.username
     
 class ReadList(models.Model):
-    readlist_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    profile_id = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    readlist = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    profile = models.OneToOneField('Profile', on_delete=models.CASCADE, related_name='readlist')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
 
     def __str__(self): 
-        return self.profile_id.username + ' listed ' + self.book_id.title
+        return self.profile.username + ' listed ' + self.book.title
     
 class BookLog(models.Model):
-    booklog_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    booklog = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
     pages_read = models.IntegerField(default=0)
     finished = models.BooleanField(default=False)
 
     def __str__(self): 
-        return self.pages_read + ' pages of '+ self.book_id.title
+        return self.pages_read + ' pages of ' + self.book.title
 
 class Library(models.Model):
-    library_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    profile_id = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    booklog_id = models.ForeignKey(BookLog, on_delete=models.CASCADE)
+    library = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    profile = models.OneToOneField('Profile', on_delete=models.CASCADE, related_name='library')
+    booklog = models.ForeignKey(BookLog, on_delete=models.CASCADE)
 
     def __str__(self): 
-        return self.profile_id.username + ' libraried ' + self.booklog_id.book_id.title   
+        return self.profile.username + ' libraried ' + self.booklog.book_id.title   
     
 class Update(models.Model):
-    update_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    booklog_id = models.ForeignKey(BookLog, on_delete=models.CASCADE)
+    update = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    booklog = models.ForeignKey(BookLog, on_delete=models.CASCADE)
     pages_today = models.IntegerField(default=0)
     date = models.DateField(auto_now_add=True)
     review = models.BooleanField(default = False)
@@ -74,4 +73,4 @@ class Update(models.Model):
     nota = models.IntegerField(blank=True, null=True)
 
     def __str__(self): 
-        return self.pages_today + ' pages today of ' + self.booklog_id.book_id.title   
+        return self.pages_today + ' pages today of ' + self.booklog.book_id.title   
